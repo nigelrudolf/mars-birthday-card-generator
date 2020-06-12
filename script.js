da662127fdb7184e341d14b3e1da30e6534a3246
PCU <PCU@PCUs-Mac-mini.local>
2020-06-12 15:45:18 -0400

    testing krys

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("CONNECTION ESTABLISHED");
+
+
+const app = {
+
+    init : function(){
+        console.log(`initiate then call collectInfo function`);
+        this.collectInfo();
+    },
+
+    collectInfo : function(){
+        console.log(`collect info from user, then call getInfo function`);
+        this.getInfo();
+    },
+
+    getInfo : function(){
+        console.log(`get info from api, then call displayInfo function`);
+        this.displayInfo();
+    },
+
+    displayInfo : function(){
+        console.log(`display info from api, then call share to social media function`);
+        this.socialMedia();
+    },
+
+    socialMedia : function(){
+        console.log(`share to social media.`);
+    
+    },
+
+}
+
+app.init();
